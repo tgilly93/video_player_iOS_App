0d65e7b0162e1e39ba21332a5350833df10147df
Terry Gilmore <tgilly93@gmail.com>
2022-09-30 11:00:00 -0400

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Terry Gilmore on 9/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
